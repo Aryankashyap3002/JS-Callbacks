c2cbefa5cb52b6a5d105cd17ad052be7bf9c3ea1
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-04-17 22:45:32 +0530

    Added codes

--- sample.js
+++ sample.js
@@ -1,13 +1,37 @@
 function fetchURL(url, fn) {
     console.log("Downloading Started from", url);
-    setTimeout(async function download() {
+    setTimeout(function download() {
         console.log("Downloaded Completed");
         let response = "Dummy Response";
         fn(response);
-        console.log("Downloading Ended");
     }, 3000);
-    
 }
-await fetchURL("www.asd.com", function downloadCallback(response) {
+
+function writeFile(data, fn) {
+    console.log("Started writing data", data);
+    setTimeout(function () {
+        console.log("Writing Completed");
+        let fileName = "output.txt"
+        fn(fileName);
+    })
+
+}
+
+function uploadFile(fileName, newUrl, fn) {
+    console.log("Uploading started");
+    setTimeout(function () {
+        console.log("File", fileName, " uploaded successfully on ", newUrl);
+        let uploadResponse = "SUCCESS";
+        fn(uploadResponse);
+    })
+}
+
+fetchURL("www.asd.com", function downloadCallback(response) {
     console.log("Downloaded response", response);
+    writeFile(response, function writeCallback(fileName) {
+        console.log("new file written is", fileName);
+        uploadFile(fileName,'www.fgh.com', function uploadCallback(uploadResponse) {
+            console.log("Successfully uploaded ", uploadResponse);
+        })
+    })
 })
